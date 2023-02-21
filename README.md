c62346a1be530506fd91b489a10c570a4e63fd48
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-02-21 13:16:23 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# macos-stock-price-notifier
+
+Notify stock price every 5 minutes.
+
+<img src="./screenshots/1.png" width="400">
+
+## Usage
+
+```bash
+brew install httpie jq pup
+sh ./stock-price.sh
+```
+
+## Prerequisites
+
+- [HTTPie – API testing client that flows with you](https://httpie.io/)
+- [jq](https://stedolan.github.io/jq/)
+- [GitHub - ericchiang/pup: Parsing HTML at the command line](https://github.com/EricChiang/pup)
---
+++ stock-price.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# macos-stock-price-notifier
+
+STOCK_URLS=(
+    'https://invest.cnyes.com/twstock/TWG/2646'
+    'https://invest.cnyes.com/twstock/TWS/3443'
+)
+while :
+do
+    for STOCK_URL in "${STOCK_URLS[@]}"
+    do
+        JSON=$(http "$STOCK_URL" | pup 'script#__NEXT_DATA__ text{}')
+        NAME=$(jq -n "$JSON" | jq -r '.props.pageProps.initialProps.quote."200009"')
+        PRICE=$(jq -n "$JSON" | jq -r '.props.pageProps.initialProps.quote."6"')
+        MESSAGE+="$NAME: $PRICE\n"
+    done
+    osascript -e "display notification \"$MESSAGE\" with title \"Stock Price Notifier\""
+    sleep 600
+done
